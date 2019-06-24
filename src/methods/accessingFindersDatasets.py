6e952f7045b541af99b06d38f5be6f0e3d254a5b
Bruno Oliveira <b.oliveira@sig.eu>
2019-06-24 15:41:42 +0200

    Refactor more

--- src/methods/accessingFindersDatasets.py
+++ src/methods/accessingFindersDatasets.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#coding=utf-8
+# coding=utf-8
 
 from baseFindersDatasets import *
 from messagesFindersDatasets import *
@@ -13,19 +13,19 @@ import json, urllib.request
 .py:
 """
 
-__author__      = "Francisco Maria Calisto"
-__maintainer__  = "Francisco Maria Calisto"
-__email__       = "francisco.calisto@tecnico.ulisboa.pt"
-__license__     = "MIT"
-__version__     = "1.0.2"
-__status__      = "Development"
-__copyright__   = "Copyright 2019, Instituto Superior Técnico (IST)"
-__credits__     = [
-  "Bruno Oliveira",
-  "Carlos Santiago",
-  "Jacinto C. Nascimento",
-  "Pedro Miraldo",
-  "Nuno Nunes"
+__author__ = "Francisco Maria Calisto"
+__maintainer__ = "Francisco Maria Calisto"
+__email__ = "francisco.calisto@tecnico.ulisboa.pt"
+__license__ = "MIT"
+__version__ = "1.0.2"
+__status__ = "Development"
+__copyright__ = "Copyright 2019, Instituto Superior Técnico (IST)"
+__credits__ = [
+    "Bruno Oliveira",
+    "Carlos Santiago",
+    "Jacinto C. Nascimento",
+    "Pedro Miraldo",
+    "Nuno Nunes"
 ]
 
 from os import path
@@ -48,33 +48,40 @@ sys.path.insert(0, varsAbsPath)
 
 time_stamp = str(int(time.time()))
 
+
 def getMedicalImages(folderToSave, mainServer, dicomServer):
-  '''
-  Downloading all medical images from your main server
-  that are stored on a DICOM server.
-  '''
-  image_counter = 10000000
-  dataStudyList = urllib.request.urlopen(mainServer).read()
-  outputStudyList = json.loads(dataStudyList)
+    '''
+    Downloading all medical images from your main server
+    that are stored on a DICOM server.
+    '''
+    image_counter = 10000000
+    dataStudyList = urllib.request.urlopen(mainServer).read()
+    outputStudyList = json.loads(dataStudyList)
 
-  patientFileOnServer = outputStudyList['studyList'].map(lambda patient: lnk004 + patient['patientId'] + ext102 )
+    patient_file_on_server = outputStudyList['studyList'].map(lambda patient: lnk004 + patient['patientId'] + ext102)
+    output_studies = patient_file_on_server.map(lambda f: json.loads(urllib.request.urlopen(f).read()))
+    series_list = output_studies.map(lambda output_study: convertToSeriesAndPrintFields(output_study))
 
-  for file in patientFileOnServer:
-    dataStudies = urllib.urlopen(file).read()
-    outputStudies = json.loads(dataStudies)
-    for study in outputStudies:
-      seriesList = study['seriesList']
-      print(msg001, study['patientId'])
-      print(msg002, study['internalId'])
-      print(msg003, study['studyId'])
-      for series in seriesList:
-        seriesNumber = series['seriesNumber']
-        instanceList = series['instanceList']
-        for instance in instanceList:
-          image_counter += 1
-          imageId = instance['imageId']
-          print(msg004, imageId, msg005, seriesNumber)
-          dcmUrl = dicomServer + imageId
-          dcmFileName = folderToSave + fn015 + image_counter + ext103
-          urllib.request.urlretrieve(dcmUrl, dcmFileName)
-    print(c010)
+    for series in series_list:
+        series_number = series['seriesNumber']
+        instance_list = series['instanceList']
+        for instance in instance_list:
+            process_instance(dicomServer, folderToSave, image_counter, instance, series_number)
+        print(c010)
+
+
+def process_instance(dicomServer, folderToSave, image_counter, instance, seriesNumber):
+    image_counter += 1
+    imageId = instance['imageId']
+    print(msg004, imageId, msg005, seriesNumber)
+    dcmUrl = dicomServer + imageId
+    dcmFileName = folderToSave + fn015 + image_counter + ext103
+    urllib.request.urlretrieve(dcmUrl, dcmFileName)
+
+
+def convertToSeriesAndPrintFields(study):
+    seriesList = study['seriesList']
+    print(msg001, study['patientId'])
+    print(msg002, study['internalId'])
+    print(msg003, study['studyId'])
+    return seriesList
