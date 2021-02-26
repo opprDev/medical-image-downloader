3fd9a38964a4d900605fd0706ddc240813dd5229
Francisco Maria Calisto <francisco_mcalisto@outlook.com>
2021-02-26 10:15:26 +0000

    [UPDATE] New Server Domain Constants

--- src/mi/constant.py
+++ src/mi/constant.py
@@ -1,4 +1,4 @@
-MI_CORNERSTONE_URL = 'http://breastscreening.isr.tecnico.ulisboa.pt:8982/src/'
+MI_CORNERSTONE_URL = 'http://breastscreening.arditi.pt:8982/src/'
 MI_STUDY_LIST_URL = MI_CORNERSTONE_URL + 'studyList.json'
 MI_STUDIES_URL = MI_CORNERSTONE_URL + 'studies/'
-MI_ORTHANC_URL = 'http://breastscreening.isr.tecnico.ulisboa.pt:8450'
+MI_ORTHANC_URL = 'http://breastscreening.arditi.pt:8450'
